f30090ac2de4758ed5b874e9436178911507f88d
sako00 <sanna.erv@hotmail.com>
2022-11-19 17:37:29 +0200

    change let

--- Pages/sanna.js
+++ Pages/sanna.js
@@ -1,5 +1,5 @@
 //Question bank
-var questionBank= [
+let questionBank= [
     {
         question : 'Paljonko on 10+5?',
         option : ['11','15','10','16'],
@@ -27,22 +27,22 @@ var questionBank= [
     }
 ]
 
-var question= document.getElementById('question');
-var quizContainer= document.getElementById('quiz-container');
-var scorecard= document.getElementById('scorecard');
-var option0= document.getElementById('option0');
-var option1= document.getElementById('option1');
-var option2= document.getElementById('option2');
-var option3= document.getElementById('option3');
-var next= document.querySelector('.next');
-var points= document.getElementById('score');
-var span= document.querySelectorAll('span');
-var i=0;
-var score= 0;
+let question= document.getElementById('question');
+let quizContainer= document.getElementById('quiz-container');
+let scorecard= document.getElementById('scorecard');
+let option0= document.getElementById('option0');
+let option1= document.getElementById('option1');
+let option2= document.getElementById('option2');
+let option3= document.getElementById('option3');
+let next= document.querySelector('.next');
+let points= document.getElementById('score');
+let span= document.querySelectorAll('span');
+let i=0;
+let score= 0;
 
 //function to display questions
 function displayQuestion(){
-    for(var a=0;a<span.length;a++){
+    for(let a=0;a<span.length;a++){
         span[a].style.background='none';
     }
     question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
@@ -90,13 +90,13 @@ function backToQuiz(){
 
 //function to check Answers
 function checkAnswer(){
-    var answerBank= document.getElementById('answerBank');
-    var answers= document.getElementById('answers');
+    let answerBank= document.getElementById('answerBank');
+    let answers= document.getElementById('answers');
     answerBank.style.display= 'block';
     scoreboard.style.display= 'none';
-    for(var a=0;a<questionBank.length;a++)
+    for(let a=0;a<questionBank.length;a++)
     {
-        var list= document.createElement('li');
+        let list= document.createElement('li');
         list.innerHTML= questionBank[a].answer;
         answers.appendChild(list);
     }
