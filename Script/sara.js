a7758b214265c1d47681523ae43795923850907b
Sara-Sofia <n2kesa00@students.oamk.fi>
2022-11-21 19:24:47 +0200

    js äikkävisaan

--- Script/sara.js
+++ Script/sara.js
@@ -1,5 +1,35 @@
-// Start buttonin toiminta:
+//Malli:https://github.com/Ahmad528/quizApp/blob/main/quizwuiz/quiz.js
 
-document.getElementById("startbutton").onclick=function(){
-    // Kysymykset alkaa tästä
-}
+//Kysymykset talletettu taulukkoon:
+
+let questions =[
+{
+    q: "Kysymys 1",
+    o: ["Vaihtoehto1, vaihtoehto2, vaihtoehto3"],
+    a: ["vaihtoehto2,"]
+},
+
+{
+    q: "kysymys 2",
+    o: ["vaihtoehto1, vaihtoehto2, vaihtoehto3"],
+    a: ["vaihtoehto3"]
+},
+
+{
+    q: "kysymys3",
+    o: ["vaihtoehto1, vaihtoehto2, vaihtoehto3"],
+    a: ["vaihtoehto3"]
+},
+
+{
+    q: "kysymys4",
+    o: ["vaihtoehto1, vaihtoehto2, vaihtoehto3"],
+    a: ["vaihtoehto1"]
+},
+
+{
+    q: "kysymys5",
+    o:["vaihtoehto1, vaihtoehto2, vaihtoehto3"],
+    a:["vaihtoehto1"]
+},
+]
