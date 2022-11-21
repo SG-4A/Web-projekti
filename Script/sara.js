5183b5028e808295657923eb72e0833b9dc6a233
Sara-Sofia <n2kesa00@students.oamk.fi>
2022-11-21 20:14:03 +0200

    pohja kyselylle

--- Script/sara.js
+++ Script/sara.js
@@ -33,3 +33,86 @@ let questions =[
     a:["vaihtoehto1"]
 },
 ]
+
+// Tarvittavat muuttujat:
+
+let question = document.getElementById("question");
+let quiz = document.getElementById("container");
+let scoreBoard = document.getElementById("scores");
+let option0 = document.getElementById("option0");
+let option1 = document.getElementById("option1");
+let option2 = document.getElementById("option2");
+let next = document.querySelector(".next");
+let finalScore = document.getElementById("yourScoreHere");
+let span = document.querySelectorAll("span");
+let i=0;
+let points=0;
+
+// functio joka näyttää kysymykset:
+
+function displayQuestion(){
+    for(let j=0; j<span.length; j++){
+        span[j].style.background='none';
+    }
+        question.innerHTML= "Q." + (i+1)+" "+questions[i].question;
+        option0.innerHTML=questions[i].option[0];
+        option1.innerHTML=questions[i].option[1];
+        option2.innerHTML=questions[i].option[2];
+        stat.innerHTML="Kysymys" + " "+(i+1)+" "+ "/" + " " +questions.length;
+    
+    }
+
+// functio tuloksen laskemiseen:
+function calculateScore(e){
+    if(e.innerHTML===questions[i].a && points<questions.length)
+{
+    points= points+1;
+    document.getElementById(e.id).style.background= "dark green";
+}
+else{
+    document.getElementById(e.id).style.background= "dark red";
+}
+setTimeout(nextQuestion,300);
+}
+
+// functio joka siirtää seuraavaan kysymykseen:
+function nextQuestion(){
+    if(i<questions.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        finalScore.innerHTML= points+ "/"+ questions.length;
+        quiz.style.display= "none";
+        scoreBoard.style.display= "block"
+    }
+    }
+
+// Eventti seuraava-painikkeelle:
+
+next.addEventListener("click",nextQuestion);
+
+//Takaisin painike:
+function backToQuiz(){
+    location.reload();
+}
+
+// Funktio jolla tarkistetaan vastaukset:
+
+function checkAnswers(){
+    let answerBank= document.getElementById("answers");
+    let answers= document.getElementById("answerList");
+    answerBank.style.display= "block";
+    scoreBoard.style.display= "none";
+    for(let j=0;j<questions.length;j++)
+    {
+        let list= document.createElement("li");
+        list.innerHTML= questions[b].a;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
+
